316b7587670b2790aa12327774911b3ee291a9ab
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-03-13 12:32:29 +0530

    routine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hey!</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <h1>Works fine</h1>
+          </Route>
+
+          <Route exact path="/better">
+            <h1>Works better</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
